15bdfb802cf85bb46a94f40aa965dfffa7fb76de
Yvonne Chen <yc.yvonnechen@yahoo.com>
2016-11-28 17:10:40 -0800

    cleanup homepage

--- resources/js/homepage.js
+++ resources/js/homepage.js
@@ -1,89 +1,98 @@
-var sectionWidth = d3.select('#section-1').node().getBoundingClientRect().width;
-var sectionHeight = d3.select('#section-1').node().getBoundingClientRect().height;
+function FlyingSquaresAnimation(){
 
-var winWidth = $(window).width();
-var winHeight = $(window).height();
+    var sectionWidth = d3.select('#section-1').node().getBoundingClientRect().width;
+    var sectionHeight = d3.select('#section-1').node().getBoundingClientRect().height;
 
-var width = 5, height = 5,
-    off = 3, buildingSpacing = 10;
-var continueSquaresMovement = true;
+    var winWidth = $(window).width();
+    var winHeight = $(window).height();
 
-var colors = ['#F8C81C', '#D0021B'];
-var colorScale = d3.scale.linear()
-                .domain([0, 1])
-                .range(['#fff', '#F8C81C']);
+    var width = 5, height = 5,
+        off = 3, buildingSpacing = 10;
+    var continueSquaresMovement = true;
 
-var background = d3.select('#section-1')
-        .append('svg')
-        .attr('width', sectionWidth)
-        .attr('height', sectionHeight);
+    var colors = ['#F8C81C', '#D0021B'];
+    var colorScale = d3.scale.linear()
+                    .domain([0, 1])
+                    .range(['#fff', '#F8C81C']);
 
-function initSquaresDataRandomely(number){
-    var data = new Array();
+    var background = d3.select('#section-1')
+            .append('svg')
+            .attr('width', sectionWidth)
+            .attr('height', sectionHeight);
 
-    for(var i = 0; i < number; i++){
-        var rand = Math.random();
+    function initSquaresDataRandomely(number){
+        var data = new Array();
 
-        data.push({
-            x: Math.random()*sectionWidth, 
-            y: (1+Math.random())*sectionHeight,
-            width: width,
-            height: height,
-            fill: '#F8C81C',          
-            //fill: colors[Math.floor(Math.random()*colors.length)],
-            //fill: colorScale(Math.random),            //smaller rand = whiter & more in front
-            duration: (rand+0.001) * 3000 + 2000,       //smaller rand = slower
-            opacity: 1 - rand
-        });
+        for(var i = 0; i < number; i++){
+            var rand = Math.random();
+
+            data.push({
+                x: Math.random()*sectionWidth, 
+                y: (1+Math.random())*sectionHeight,
+                width: width,
+                height: height,
+                fill: '#F8C81C',          
+                //fill: colors[Math.floor(Math.random()*colors.length)],
+                //fill: colorScale(Math.random),            //smaller rand = whiter & more in front
+                duration: (rand+0.001) * 3000 + 2000,       //smaller rand = slower
+                opacity: 1 - rand
+            });
+        }
+
+        return data;
     }
 
-    return data;
+    function initSquaresRandomly(number){
+
+        var squaresData = initSquaresDataRandomely(number);
+
+        background.append('g').selectAll('rect')
+            .data(squaresData)
+            .enter()
+            .append('rect')
+                .attr('x', function(d) {return d.x;})
+                .attr('y', function(d) {return d.y;})
+                .attr('width', function(d) {return d.width;})
+                .attr('height', function(d) {return d.height;})
+                .style('fill', function(d) {return d.fill;})
+                .style('opacity', function(d) {return d.opacity;})
+                .transition()
+                    .each('start', function upwardsRepeat(){
+                        if(continueSquaresMovement){
+                            d3.select(this)
+                                .transition()
+                                    .duration(function(d){return d.duration})
+                                    .ease('linear')
+                                    .attr('y', '-200')
+                                .transition()
+                                    .duration(0)
+                                    .attr('y', sectionHeight + 50)
+                                .transition()
+                                    .each('start', upwardsRepeat);
+                        }
+                    });
+    }
+
+    initSquaresRandomly(300);
+
 }
 
-function initSquaresRandomly(number){
-    var squaresData = initSquaresDataRandomely(number);
+function handleNavUpdate(){
+    var top = $(document).scrollTop();
+    var sectionArray = ($('[id^="section"]').toArray());
 
-    background.append('g').selectAll('rect')
-        .data(squaresData)
-        .enter()
-        .append('rect')
-            .attr('x', function(d) {return d.x;})
-            .attr('y', function(d) {return d.y;})
-            .attr('width', function(d) {return d.width;})
-            .attr('height', function(d) {return d.height;})
-            .style('fill', function(d) {return d.fill;})
-            .style('opacity', function(d) {return d.opacity;})
-            .transition()
-                .each('start', function upwardsRepeat(){
-                    if(continueSquaresMovement){
-                        d3.select(this)
-                            .transition()
-                                .duration(function(d){return d.duration})
-                                .ease('linear')
-                                .attr('y', '-200')
-                            .transition()
-                                .duration(0)
-                                .attr('y', sectionHeight + 50)
-                            .transition()
-                                .each('start', upwardsRepeat);
-                    }
-                });
-}
+    sectionArray.forEach(function(section) {
+        if ($(section).hasClass('skrollable-between')) {
+            var activeSessionID = '#' + $(section).attr('id') + '-a';
 
-initSquaresRandomly(300);
+            $('.nav-box').removeClass('active');
+            $('a[href="' + activeSessionID + '"]').addClass('active');
 
-$(document).ready(function() {
-
-    $('.pre-over').delay(100).fadeOut();
-
-    var s = skrollr.init({
-        forceHeight: false,
-        smoothScrolling: true,
+        }
     });
+}
 
-    /*NAVIGATION START*/
-    
-    //smooth scroll
+function enableSmoothScroll(){
     $(function() {
       $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -98,35 +107,31 @@ $(document).ready(function() {
         }
       });
     });
+}
 
-    var sectionArray = [];
-    var activeSessionID = '';
+$(document).ready(function() {
 
-    $(window).scroll(function() {
+    handleNavUpdate();
+    FlyingSquaresAnimation();
 
-        var top = $(document).scrollTop();
-        sectionArray = ($('[id^="section"]').toArray());
-
-        sectionArray.forEach(function(section) {
-            if ($(section).hasClass('skrollable-between')) {
-                activeSessionID = '#' + $(section).attr('id') + '-a';
-
-                $('.nav-box').removeClass('active');
-                $('a[href="' + activeSessionID + '"]').addClass('active');
-
-            }
-        });
+    var s = skrollr.init({
+        forceHeight: false,
+        smoothScrolling: true,
     });
+    
+    enableSmoothScroll();
+    $(window).scroll(handleNavUpdate);
 
-    /*NAVIGATION END*/
 
+    //various animations
+    $('.pre-over').delay(100).fadeOut();
     $('.title-text-wrapper').delay(2000)
                             .animate({
                                 'margin-top': '-100px',
                                 'opacity' : 1
                             }, 1000);  
 
-    $('.info-box').mouseover(function(){
+    $('.explore').mouseover(function(){
         allChange(flyRight);
     }).mouseout(function(){
         allChange(changeBack);
--- resources/js/squares-over.js
+++ resources/js/squares-over.js
@@ -1,119 +1,122 @@
-var canvas = d3.select('.fixed-over-squares');
+var SquaresAnimator = (function () {
+    var canvas = d3.select('.fixed-over-squares');
 
-var canvasWidth = canvas.node().getBoundingClientRect().width;
-var canvasHeight = canvas.node().getBoundingClientRect().height;
+    var canvasWidth = canvas.node().getBoundingClientRect().width;
+    var canvasHeight = canvas.node().getBoundingClientRect().height;
 
-var width = 15, height = 15;
+    var width = 15, height = 15;
 
-function initGrid(gridHeight, gridWidth) {
-    var data = new Array();
-    var xpos = 1, ypos = 1;
+    function initGrid(gridHeight, gridWidth) {
+        var data = new Array();
+        var xpos = 1, ypos = 1;
 
-    var xOffset = 0,
-        yOffset = 0;
-    var xTrans = 0,
-        yTrans = 0;
+        var xOffset = 0,
+            yOffset = 0;
+        var xTrans = 0,
+            yTrans = 0;
 
-    for (var row = 0; row < gridHeight/height; row++) {
-        data.push(new Array());
-        for (var col = 0; col < gridWidth/width; col++) {
-            data[row].push({
-                x: xpos,
-                y: ypos,
-                width: width,
-                height: height,
-                xOffset: xOffset,
-                yOffset: yOffset,
-                xTrans: xTrans,
-                yTrans: yTrans
-            })
-            xpos += width;
+        for (var row = 0; row < gridHeight/height; row++) {
+            data.push(new Array());
+            for (var col = 0; col < gridWidth/width; col++) {
+                data[row].push({
+                    x: xpos,
+                    y: ypos,
+                    width: width,
+                    height: height,
+                    xOffset: xOffset,
+                    yOffset: yOffset,
+                    xTrans: xTrans,
+                    yTrans: yTrans
+                })
+                xpos += width;
+            }
+            xOffset = 0;
+            xpos = 1;
+            ypos += height;
         }
-        xOffset = 0;
-        xpos = 1;
-        ypos += height;
+        return data;
     }
-    return data;
-}
 
-function flyOut(d){
-    var xOff = 5/(Math.random() - 0.5);
-    var yOff = 5/(Math.random() - 0.5);
-    d3.select(this)
-        .attr('xTrans', function(d){return xOff;})
-        .attr('yTrans', function(d){return yOff;})
-        .transition()
-        .ease('cubic')
-        .duration(200)
-            .attr('transform', 'translate(' + xOff + ', ' + yOff + ')')
-            .style('fill', 'rgba(0,0,0,1)')
-            .style('z-index', '100');
-}
+    function flyOut(d){
+        var xOff = 5/(Math.random() - 0.5);
+        var yOff = 5/(Math.random() - 0.5);
+        d3.select(this)
+            .attr('xTrans', function(d){return xOff;})
+            .attr('yTrans', function(d){return yOff;})
+            .transition()
+            .ease('cubic')
+            .duration(200)
+                .attr('transform', 'translate(' + xOff + ', ' + yOff + ')')
+                .style('fill', 'rgba(0,0,0,1)')
+                .style('z-index', '100');
+    }
 
-function flyIn(d){
-    d3.select(this)
-        .transition()
-        .ease('cubic')
-        .delay(200)
-        .duration(200)
-            .attr('transform', function(d){
-                return 'translate(0,0)';
-            })
-            .attr('xTrans', 0)
-            .attr('yTrans', 0)
-            .style('fill', 'rgba(0,0,0,0.5)')
-            .style('z-index', '1');
-}
-function initAll(){
-
-    var gridData = initGrid(canvasHeight, canvasWidth);
-
-    var row = grid.append('svg')
-                .attr('height', canvasHeight)
-                .attr('width', canvasWidth)
-                .attr('x', 0)
-                .attr('y', 0)
-            .selectAll('.row')
-            .data(gridData)
-            .enter().append('g')
-                .attr('class', 'row');
-
-    var col = row.selectAll('rect')
-            .data(function(d) {return d;})
-            .enter().append('rect')
-                //.attr('id', function(d, i) {return 'rect' + i;})
-                .attr('x', function(d) {return d.x + d.xOffset;})
-                .attr('y', function(d) {return d.y + d.yOffset;})
-                .attr('width', function(d) {return d.width;})
-                .attr('height', function(d) {return d.height;})
+    function flyIn(d){
+        d3.select(this)
+            .transition()
+            .ease('cubic')
+            .delay(200)
+            .duration(200)
+                .attr('transform', function(d){
+                    return 'translate(0,0)';
+                })
+                .attr('xTrans', 0)
+                .attr('yTrans', 0)
                 .style('fill', 'rgba(0,0,0,0.5)')
-                .on('mouseover', flyOut)
-                .on('mouseout', flyIn);
-}
+                .style('z-index', '1');
+    }
+    function initAll(){
 
-function flyRight(){
-    d3.select(this)
-        .transition()
-        .duration(1000)
-            .attr('transform', 'translate(2000, 0)');
-}
+        var gridData = initGrid(canvasHeight, canvasWidth);
 
-function changeBack(){
-    d3.select(this)
-        .transition()
-        .duration(500)
-        .attr('transform', 'translate(0, 0)')
-        .style('fill', 'rgba(0,0,0,0.5)');
-}
+        var row = grid.append('svg')
+                    .attr('height', canvasHeight)
+                    .attr('width', canvasWidth)
+                    .attr('x', 0)
+                    .attr('y', 0)
+                .selectAll('.row')
+                .data(gridData)
+                .enter().append('g')
+                    .attr('class', 'row');
 
-function allChange(change){
-    d3.selectAll('.fixed-over-squares rect').each(change);
-}
+        var col = row.selectAll('rect')
+                .data(function(d) {return d;})
+                .enter().append('rect')
+                    //.attr('id', function(d, i) {return 'rect' + i;})
+                    .attr('x', function(d) {return d.x + d.xOffset;})
+                    .attr('y', function(d) {return d.y + d.yOffset;})
+                    .attr('width', function(d) {return d.width;})
+                    .attr('height', function(d) {return d.height;})
+                    .style('fill', 'rgba(0,0,0,0.5)')
+                    .on('mouseover', flyOut)
+                    .on('mouseout', flyIn);
+    }
 
-var grid = canvas
-    .append('svg')
-    .attr('width', canvasWidth)
-    .attr('height', canvasHeight)
-    .classed('canvas', true);
+    function flyRight(){
+        d3.select(this)
+            .transition()
+            .duration(1000)
+                .attr('transform', 'translate(2000, 0)');
+    }
 
-initAll();
+    function changeBack(){
+        d3.select(this)
+            .transition()
+            .duration(500)
+            .attr('transform', 'translate(0, 0)')
+            .style('fill', 'rgba(0,0,0,0.5)');
+    }
+
+    function allChange(change){
+        d3.selectAll('.fixed-over-squares rect').each(change);
+    }
+
+    var grid = canvas
+        .append('svg')
+        .attr('width', canvasWidth)
+        .attr('height', canvasHeight)
+        .classed('canvas', true);
+
+    initAll();
+    
+})();
